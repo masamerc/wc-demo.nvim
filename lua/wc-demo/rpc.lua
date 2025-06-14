2c91815bfe261e90abb0322441b7e828d98c0b50
Masa Fukui <masahiro.m.fukui@gmail.com>
2025-06-15 00:01:35 +0900

    update: code comments

--- lua/wc-demo/rpc.lua
+++ lua/wc-demo/rpc.lua
@@ -8,30 +8,31 @@ M.setup = function(opts)
 	local rpc_dir = plugin_path .. "/rpc"
 	local binary_path = rpc_dir .. "/bin/server"
 
+    -- [[
+    --     Helper Functions
+    -- ]]
 
-	local function build_binary()
-		local build_cmd = { "go", "build", "-o", binary_path, "." }
-		local build_job = vim.fn.jobstart(build_cmd, {
-			cwd = rpc_dir,
-			on_exit = function(_, exit_code)
-				if exit_code ~= 0 then
-					vim.notify("Failed to build RPC binary", vim.log.levels.ERROR)
-				end
-			end,
-		})
-		vim.fn.jobwait({ build_job })
-	end
-
+    -- build the rpc binary if it doesn't exist
 	local function ensure_binary()
+		local build_cmd = { "go", "build", "-o", binary_path, "." }
 		if vim.fn.filereadable(binary_path) == 0 then
-			build_binary()
+            local build_job = vim.fn.jobstart(build_cmd, {
+                cwd = rpc_dir,
+                on_exit = function(_, exit_code)
+                    if exit_code ~= 0 then
+                        vim.notify("Failed to build RPC binary", vim.log.levels.ERROR)
+                    end
+                end,
+            })
+		vim.fn.jobwait({ build_job })
 		end
 	end
 
 	-- stores the rpc job id for caching
 	local chan
 
-    -- if we already have a job, return it
+    -- check if the rpc binary exists & if there is an existing job 
+    -- if not create a new one
 	local function ensure_job()
 		if chan then
 			return chan
@@ -41,12 +42,17 @@ M.setup = function(opts)
 		return chan
 	end
 
-    -- register user commands
+    -- [[
+    --     Register Commands
+    -- ]]
+
+    -- 1: Hello: just greet the caller
 	vim.api.nvim_create_user_command("Hello", function(args)
 		vim.fn.rpcrequest(ensure_job(), "hello", args.fargs)
 	end, { nargs = "*" })
 
-	vim.api.nvim_create_user_command("Wc", function(args)
+    -- 2: Wc: word count
+	vim.api.nvim_create_user_command("Wc", function()
 		local text_to_analyze = ""
 
 		-- get visual selection
